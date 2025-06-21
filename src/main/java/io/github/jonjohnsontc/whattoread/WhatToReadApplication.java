d03000c81a22df46d4bb0df7ece456e2274d3bb8
Jon Johnson <jonjohnsontc@live.com>
2025-06-21 14:04:10 -0700

    initial commit

---
+++ src/main/java/io/github/jonjohnsontc/whattoread/WhatToReadApplication.java
@@ -0,0 +1,15 @@
+package io.github.jonjohnsontc.whattoread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+// Disabling DataSource / db configuration temporarily
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class WhatToReadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatToReadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/jonjohnsontc/whattoread/WhatToReadApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jonjohnsontc.whattoread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatToReadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
